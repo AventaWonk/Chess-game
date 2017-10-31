35b8e1546d36e002352e666bbf6afcb92a25eeee
Roman <aventawonk@gmail.com>
2017-10-31 22:48:27 +0300

    Update ChessGame class
    + Coordinate
    + virtualBoardPoint
    + BoardPoint
    + setPieceOnPoint

--- src/ChessGame.ts
+++ src/ChessGame.ts
@@ -1,27 +1,147 @@
-interface BoardPoint {
+import Piece from './Piece/Piece';
+import Pawn from './Piece/Pawn';
+
+interface Coordinate {
+  i: number;
+  j: number;
+}
+
+interface virtualBoardPoint {
   piece: any;
+  coordinate: Coordinate;
+}
+
+interface BoardPoint {
+  piece: Piece;
   squareLink: HTMLElement;
 }
 
 export default class ChessGame {
-  private static SQUARE_SIZE = "60";
-  private static WHITE_SQUARE_COLOR = "#ffccb3";
-  private static BLACK_SQUARE_COLOR = "#af6744";
   // public static PLAYER_WHTIE = 1;
   // public static PLAYER_BLACK = 2;
+  private static SQUARE_SIZE = "60";
+  private static IMAGE_SIZE = "40";
+  private static WHITE_SQUARE_COLOR = "#ffccb3";
+  private static BLACK_SQUARE_COLOR = "#af6744";
+  private selectedPiece: virtualBoardPoint = null;
   private chessBoard: BoardPoint[][] = Array(8);
+  private chessBoardElement: HTMLElement;
 
   constructor() {
     this.chessBoard = [[],[],[],[],[],[],[],[]];
     this.onBoardClickCallback = this.onBoardClickCallback.bind(this);
+    this.chessBoardElement = this.createChessBoardElement();
+    let piecesSetup = [
+      {
+        piece: new Pawn(),
+        coordinate: {
+          i: 1,
+          j: 0,
+        },
+      },
+      {
+        piece: new Pawn(),
+        coordinate: {
+          i: 1,
+          j: 1,
+        },
+      },
+      {
+        piece: new Pawn(),
+        coordinate: {
+          i: 1,
+          j: 2,
+        },
+      },
+      {
+        piece: new Pawn(),
+        coordinate: {
+          i: 1,
+          j: 3,
+        },
+      }
+    ]
+    this.setUpPiecesOnBoard(piecesSetup);
+
   }
 
   private onBoardClickCallback(e: Event) {
-    let i = parseInt((e.target as HTMLElement).dataset.i, 10);
-    let j = parseInt((e.target as HTMLElement).dataset.j, 10);
+    let target = e.target as HTMLElement;
 
-    if (i != NaN && j != NaN) {
-      //
+    if (target.nodeName != "TD") {
+      target = target.parentNode as HTMLElement;
+    }
+
+    let i: number, j: number;
+    if (!target.dataset.i || !target.dataset.j) {
+      return;
+    }
+
+    i = parseInt(target.dataset.i, 10);
+    j = parseInt(target.dataset.j, 10);
+    let current = this.chessBoard[i][j];
+    let coordinate = {
+      i: i,
+      j: j,
+    };
+
+    if (this.selectedPiece) {
+      let selected = this.chessBoard[this.selectedPiece.coordinate.i][this.selectedPiece.coordinate.j];
+      this.removePiecefromPoint(this.selectedPiece.coordinate);
+      this.setPieceOnPoint(coordinate, this.selectedPiece.piece);
+      selected.squareLink.style.background = "#ffccb3";
+      this.selectedPiece = null;
+    } else {
+      if (current.piece) {
+        this.selectedPiece = {
+          piece: current.piece,
+          coordinate: coordinate,
+        }
+        current.squareLink.style.background = "#ffcaa1";
+      }
+    }
+
+  }
+
+  private onKeyClickCallback(e: Event) {
+    if (this.selectedPiece) {
+      this.selectedPiece = null;
+    }
+  }
+
+  private move(piece: virtualBoardPoint, coordinate: Coordinate): void {
+
+  }
+
+  private removePieceImgfromSquare(squareLink: HTMLElement) {
+    if (squareLink.childElementCount) {
+      squareLink.removeChild(squareLink.lastChild);
+    }
+  }
+
+  private setUpPieceImgOnSquare(squareLink: HTMLElement, piece: Piece) {
+    if (squareLink.childElementCount) {
+      squareLink.removeChild(squareLink.lastChild);
+    }
+    squareLink.appendChild(piece.getImage(ChessGame.IMAGE_SIZE));
+  }
+
+  private removePiecefromPoint(coordinate: Coordinate) {
+    let currentSquare = this.chessBoard[coordinate.i][coordinate.j];
+    this.removePieceImgfromSquare(currentSquare.squareLink);
+    currentSquare.piece = null;
+  }
+
+  private setPieceOnPoint(coordinate: Coordinate, piece: Piece) {
+    let currentSquare = this.chessBoard[coordinate.i][coordinate.j];
+    this.setUpPieceImgOnSquare(currentSquare.squareLink, piece)
+    currentSquare.piece = piece;
+  }
+
+  private setUpPiecesOnBoard(a: virtualBoardPoint[]) {
+    for (let i = 0; i < a.length; i++) {
+      let currentSquare = this.chessBoard[a[i].coordinate.i][a[i].coordinate.j];
+      this.setPieceOnPoint(a[i].coordinate, a[i].piece);
     }
   }
 
@@ -41,21 +161,7 @@ export default class ChessGame {
     }
   }
 
-  private setUpPieceOnSquare(boardPoint: BoardPoint, piece: any) {
-    if (boardPoint.piece) {
-      boardPoint.squareLink.removeChild(boardPoint.squareLink.lastChild);
-    }
-    boardPoint.squareLink.appendChild(piece.getImage());
-    boardPoint.piece = piece;
-  }
-
-  private setUpPiecesOnBoard(a: any) {
-    for (let i = 0; i < a.length; i++) {
-
-    }
-  }
-
-  public createChessBoard() {
+  private createChessBoardElement() {
     let boardElement = document.createElement("table");
     boardElement.setAttribute("style", "border-collapse:collapse;");
 
@@ -71,10 +177,11 @@ export default class ChessGame {
             square.style.height = ChessGame.SQUARE_SIZE + "px";
             square.style.width = ChessGame.SQUARE_SIZE + "px";
             square.style.background = color;
+            square.style.textAlign = "center";
 
             line.appendChild(square);
             this.chessBoard[i][j] = {
-              piece: 0,
+              piece: null,
               squareLink: square,
             }
         }
@@ -83,5 +190,9 @@ export default class ChessGame {
 
     boardElement.addEventListener("click", this.onBoardClickCallback);
     return boardElement;
+  }
+
+  public getChessBoard() {
+    return this.chessBoardElement;
   }
 }
