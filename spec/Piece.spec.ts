9b6c74324d2b4e7e7abbe923cbef6b57bda95dfc
Roman <aventawonk@gmail.com>
2018-01-25 20:18:03 +0300

    Update pieces tests

--- spec/Piece.spec.ts
+++ spec/Piece.spec.ts
@@ -1,5 +1,4 @@
 import {Coordinate} from '../src/Types/Coordinate';
-import {PiecesSetup} from '../src/Types/PiecesSetup';
 import Piece from '../src/Piece/Piece';
 import Bishop from '../src/Piece/Bishop';
 import King from '../src/Piece/King';
@@ -11,55 +10,37 @@ import VirtualChessboard from '../src/VirtualChessboard';
 
 describe("Chess pieces classes test", () => {
   let vcb = new VirtualChessboard();
-  let bishop = new Bishop(0);
-  let king = new King(0);
-  let knight = new Knight(0);
-  let pawn = new Pawn(0);
-  let queen = new Queen(0);
-  let rook = new Rook(0);
-  let piecesSetUp: PiecesSetup[] = [
-    {
-      piece: bishop,
-      coordinate: {
-        x: 1,
-        y: 1
-      }
-    },
-    {
-      piece: king,
-      coordinate: {
-        x: 2,
-        y: 2
-      }
-    },
-    {
-      piece: knight,
-      coordinate: {
-        x: 3,
-        y: 3
-      }
-    },
-    {
-      piece: pawn,
-      coordinate: {
-        x: 4,
-        y: 4
-      }
-    },
-    {
-      piece: queen,
-      coordinate: {
-        x: 5,
-        y: 5
-      }
-    },
-    {
-      piece: rook,
-      coordinate: {
-        x: 6,
-        y: 6
-      }
-    },
+  let bishop = new Bishop(0, {
+    x: 0,
+    y: 0
+  });
+  let king = new King(0, {
+    x: 1,
+    y: 1
+  });
+  let knight = new Knight(0, {
+    x: 2,
+    y: 2
+  });
+  let pawn = new Pawn(0, {
+    x: 3,
+    y: 3
+  });
+  let queen = new Queen(0, {
+    x: 4,
+    y: 4
+  });
+  let rook = new Rook(0, {
+    x: 5,
+    y: 5
+  });
+  let piecesSetUp: Piece[] = [
+    bishop,
+    king,
+    knight,
+    pawn,
+    queen,
+    rook
   ];
   vcb.setUpPieces(piecesSetUp);
 
@@ -68,6 +49,67 @@ describe("Chess pieces classes test", () => {
       {
         x: 1,
         y: 1
+      },
+      {
+        x: 2,
+        y: 2
+      },
+      {
+        x: 3,
+        y: 3
+      },
+      {
+        x: 4,
+        y: 4
+      },
+      {
+        x: 5,
+        y: 5
+      },
+      {
+        x: 6,
+        y: 6
+      },
+      {
+        x: 7,
+        y: 7
+      },
+    ]);
+  });
+
+  it("checks King piece", () => {
+    expect(king.getMoves()).toEqual([
+      {
+        x: 0,
+        y: 0
+      },
+      {
+        x: 0,
+        y: 2
+      },
+      {
+        x: 0,
+        y: 1
+      },
+      {
+        x: 1,
+        y: 0
+      },
+      {
+        x: 2,
+        y: 2
+      },
+      {
+        x: 2,
+        y: 0
+      },
+      {
+        x: 2,
+        y: 1
+      },
+      {
+        x: 1,
+        y: 2
       }
     ]);
   });
