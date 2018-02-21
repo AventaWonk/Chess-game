bdc5f3d76bd9c1794e0521593f8b3cdab9b23ea4
Roman <aventawonk@gmail.com>
2018-02-21 21:24:50 +0300

    Update configs

--- webpack.config.js
+++ webpack.config.js
@@ -3,30 +3,48 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    game: './src/ChessGame.ts',
-    engine: './src/ChessEngine.ts',
+    // game: './src/ChessGame.ts',
+    // engine: './src/ChessEngine.ts',
     dev: './dev/index.ts',
+    style: './src/scss/main.scss',
+    gui: './GUI/src/index.tsx',
   },
-  target: "node",
+  target: 'web',
   output: {
     filename: '[name].js',
-    path: path.resolve(__dirname, './built/dist/')
+    path: path.resolve(__dirname, './dev/'),
   },
   plugins: [
-    new webpack.optimize.ModuleConcatenationPlugin()
+    new webpack.optimize.ModuleConcatenationPlugin(),
   ],
-  devtool: "source-map",
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.json'],
+  },
   module: {
     rules: [
       {
-        test: /\.ts$/,
+        test: /\.tsx?$/,
         exclude: /(node_modules)/,
-        use: 'awesome-typescript-loader'
+        use: 'awesome-typescript-loader',
       },
-      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
-    ]
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [
+                require('precss'),
+                require('autoprefixer'),
+              ],
+            },
+          },
+        ],
+      },
+    ],
   },
-  resolve: {
-    extensions: ['.ts', '.js', '.json']
-  }
 };
