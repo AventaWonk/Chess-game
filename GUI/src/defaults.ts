afef07c4ae7a3515bdcab2f1b6001e9b9730affd
Roman <aventawonk@gmail.com>
2018-02-28 23:49:55 +0300

    Add constants

---
+++ GUI/src/defaults.ts
@@ -0,0 +1,13 @@
+export const playerId = {
+  WHITE: 0,
+  BLACK: 1,
+}
+export const color = {
+   DEFAULT_WHITE_SQUARE_COLOR: '#ffccb3',
+   DEFAULT_BLACK_SQUARE_COLOR: '#af6744',
+   DEFAULT_HIGHLIGHTED_SQUARE_COLOR: '#8a7e78',
+}
+export const size = {
+  DEFAULT_SQUARE_SIZE: 60,
+  DEFAULT_IMAGE_SIZE: 40,
+}
