aff1a6258fa41261d5a29bbd5800131f7c39d4f7
Roman <aventawonk@gmail.com>
2018-03-03 23:36:23 +0300

    Update chessboard class

--- GUI/src/components/Chessboard.tsx
+++ GUI/src/components/Chessboard.tsx
@@ -1,12 +1,13 @@
 import * as React from "react";
-import {size, color} from '../defaults'
+import {size, color, playerId, NOTATION_LETTERS} from '../defaults'
 import Square from './Square';
 // import {Point, BoardPoint} from '../types/Coordinate';
 // import {Move} from '../types/Move';
 // import {AbstractPiece, Bishop, King, Knight, Pawn, Queen, Rook} from '../types/Piece';
 
 export interface ChessboardProps {
-
+  side: number;
+  pieces: any[];
 }
 
 interface ChessboardState {
@@ -19,14 +20,62 @@ export default class Chessboard extends React.Component<ChessboardProps, Chessbo
   private blackSquareColor = color.DEFAULT_BLACK_SQUARE_COLOR;
   private highlightColor = color.DEFAULT_HIGHLIGHTED_SQUARE_COLOR;
   private squareSize = size.DEFAULT_SQUARE_SIZE;
-  private notationLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+  private chessBoardElement: any[] = [];
 
   constructor(props: ChessboardProps) {
     super(props);
+
+    if (props.side == playerId.BLACK) {
+      let rowSquares: any[] = [];
+
+      for (let i = 0; i < 8; i++) {
+        rowSquares.push(
+          <th>{i + 1}</th>
+        );
+        for (let j = 7; j > -1; j--) {
+          rowSquares.push(
+            <Square x={j} y={i - 1} color={this.getSquareColor(i, j)} size={this.squareSize} key={(i+1) * (j+64)}/>
+          );
+        }
+        this.chessBoardElement.push(
+          <tr key={i}>{rowSquares}</tr>
+        );
+        rowSquares = [];
+      }
+
+      rowSquares.push(
+        <th></th>
+      );
+      for (let i = 7; i > -1; i--) {
+        rowSquares.push(
+          <th>{NOTATION_LETTERS[i]}</th>
+        );
+      }
+      this.chessBoardElement.push(
+        <tr key={99}>{rowSquares}</tr>
+      );
+
+
+
+    } else {
+      for (let i = 7; i > -1; i--) {
+        let rowSquares: any[] = [];
+
+        for (let j = 0; j < 8; j++) {
+
+          rowSquares.push(
+            <Square x={j} y={i - 1} color={this.getSquareColor(i, j)} size={this.squareSize} key={(i+1) * (j+64)}/>
+          );
+        }
+        this.chessBoardElement.push(
+          <tr key={i}>{rowSquares}</tr>
+        );
+      }
+    }
   }
 
   private getSquareColor(x: number, y: number): string {
-    if (x % 2 == y % 2) {
+    if (x % 2 != y % 2) {
       return this.whiteSquareColor;
     }
 
@@ -34,22 +83,6 @@ export default class Chessboard extends React.Component<ChessboardProps, Chessbo
   }
 
   render() {
-    let chessBoardElement = [];
-
-    for (let i = 7; i > -1; i--) {
-      let rowSquares: any[] = [];
-
-      for (let j = 0; j < 8; j++) {
-        let squareColor = this.getSquareColor(i, j);
-
-        rowSquares.push(
-          <Square x={j} y={i - 1} color={squareColor} size={this.squareSize} key={(i+1) * (j+64)}/>
-        );
-      }
-      chessBoardElement.push(
-        <tr key={i}>{rowSquares}</tr>
-      );
-    }
     let tableStyle = {
       borderCollapse: "collapse",
     }
@@ -57,7 +90,7 @@ export default class Chessboard extends React.Component<ChessboardProps, Chessbo
     return (
       <table style={tableStyle}>
         <tbody>
-          {chessBoardElement}
+          {this.chessBoardElement}
         </tbody>
       </table>
     );
