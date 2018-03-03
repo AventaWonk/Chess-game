3ef9fd39d128e2d62b41577efcb68c488898f707
Roman <aventawonk@gmail.com>
2018-03-03 23:34:32 +0300

    Update constants

--- GUI/src/defaults.ts
+++ GUI/src/defaults.ts
@@ -1,13 +1,17 @@
 export const playerId = {
   WHITE: 0,
   BLACK: 1,
-}
+};
 export const color = {
    DEFAULT_WHITE_SQUARE_COLOR: '#ffccb3',
    DEFAULT_BLACK_SQUARE_COLOR: '#af6744',
    DEFAULT_HIGHLIGHTED_SQUARE_COLOR: '#8a7e78',
-}
+};
 export const size = {
   DEFAULT_SQUARE_SIZE: 60,
   DEFAULT_IMAGE_SIZE: 40,
-}
+};
+export const NOTATION_LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+export const DEFAULT_PIECE_SETUP = [
+  1
+];
