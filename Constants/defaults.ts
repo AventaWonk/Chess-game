be33f95c657d79e49ba85ec9c5a31ef91269e5f3
Roman <aventawonk@gmail.com>
2018-03-11 23:50:25 +0300

    Update default piece setup

--- Constants/defaults.ts
+++ Constants/defaults.ts
@@ -1,3 +1,6 @@
+import {Bishop, King, Knight, Pawn, Queen, Rook} from '../Engine/src/Piece';
+import {IPiece} from '../Interfaces/Piece';
+
 export const playerId = {
   WHITE: 0,
   BLACK: 1,
@@ -12,6 +15,17 @@ export const size = {
   DEFAULT_IMAGE_SIZE: 40,
 };
 export const NOTATION_LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-export const DEFAULT_PIECE_SETUP = [
-  1
+export const DEFAULT_PIECE_SETUP: IPiece[] = [
+  new Pawn(playerId.WHITE, {
+    x: 3,
+    y: 0
+  }),
+  new Pawn(playerId.WHITE, {
+    x: 4,
+    y: 0
+  }),
+  new Bishop(playerId.BLACK, {
+    x: 6,
+    y: 6
+  }),
 ];
