b0f5fe17c3348369e2dfc57f657fe4b86038c08a
Roman <aventawonk@gmail.com>
2018-04-01 23:59:28 +0300

    Add handleIntervalTick method to chess clock component

--- GUI/src/components/ChessClock.tsx
+++ GUI/src/components/ChessClock.tsx
@@ -13,10 +13,13 @@ interface ChessClockState {
   time: Time
 }
 
-class ChessClock extends React.Component<ChessClockProps, ChessClockState> {
+export default class ChessClock extends React.Component<ChessClockProps, ChessClockState> {
+  private startTime: number;
 
   constructor(props: ChessClockProps) {
     super(props);
+    this.startTime = new Date().getTime();
+    this.handleIntervalTick = this.handleIntervalTick.bind(this);
     this.state = {
       time: {
         hours: 0,
@@ -24,6 +27,22 @@ class ChessClock extends React.Component<ChessClockProps, ChessClockState> {
         seconds: 0,
       }
     }
+    setInterval(this.handleIntervalTick, 1000);
+  }
+
+  handleIntervalTick() {
+    let secondsPassed = Math.round((new Date().getTime() - this.startTime) / 1000);
+    let hours = Math.floor(secondsPassed / 3600);
+    let minutes = Math.floor((secondsPassed - hours * 60) / 60);
+    let seconds = Math.floor((hours * 60 + minutes * 60) - secondsPassed);
+
+    this.setState({
+      time: {
+        hours: hours,
+        minutes: minutes,
+        seconds: seconds,
+      }
+    })
   }
 
   componentWillReceiveProps(nextProps: ChessClockProps) {
@@ -33,8 +52,8 @@ class ChessClock extends React.Component<ChessClockProps, ChessClockState> {
   render() {
     return (
       <div>
-        {this.state.time.hours} ::
-        {this.state.time.minutes} ::
+        {this.state.time.hours} :
+        {this.state.time.minutes} :
         {this.state.time.seconds}
       </div>
     );
