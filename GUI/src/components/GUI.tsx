c403bdc3539cb88e9367193d922e519ccaab9e4f
Roman <aventawonk@gmail.com>
2018-04-09 23:47:01 +0300

    Update GUI component
    + handleTimeLeft
    + handleMove

--- GUI/src/components/GUI.tsx
+++ GUI/src/components/GUI.tsx
@@ -1,10 +1,15 @@
 import * as React from "react";
 import Chessboard from './Chessboard';
+import ChessClock from './ChessClock';
 import {playerId, DEFAULT_PIECE_SETUP, DEFAULT_CHESS_ENGINE} from '../../../Constants/defaults';
 import {IChessEngine} from '../../../Interfaces/ChessEngine';
 import {IPiece} from '../../../Interfaces/Piece';
+import {PieceSet} from '../../../Engine/src/PieceSet';
 
-export interface GUIProps {}
+interface GUIProps {
+
+}
+
 interface GUIState {
   isGameOver: boolean,
 }
@@ -13,6 +18,7 @@ export default class GUI extends React.Component<GUIProps, GUIState> {
   private player: number;
   private pieceSetup: IPiece[];
   private chessEngine: IChessEngine;
+  private lastChessClockTime: number;
 
   constructor(props: GUIProps) {
     super(props)
@@ -20,11 +26,20 @@ export default class GUI extends React.Component<GUIProps, GUIState> {
     this.pieceSetup = DEFAULT_PIECE_SETUP;
     this.chessEngine = new DEFAULT_CHESS_ENGINE();
     this.chessEngine.setUpPieces(this.pieceSetup);
+    this.lastChessClockTime = null;
     this.handleGameOver = this.handleGameOver.bind(this);
     this.handleNewGameStart = this.handleNewGameStart.bind(this);
+    this.handleGameSave = this.handleGameSave.bind(this);
+    this.handleTimeLeft = this.handleTimeLeft.bind(this);
+    this.handleMoveDone = this.handleMoveDone.bind(this);
+    this.handleChessClockTimerTick = this.handleChessClockTimerTick.bind(this);
     this.state = {
       isGameOver: false,
     }
+  }
+
+  handleGameSave() {
+
   }
 
   handleNewGameStart() {
@@ -39,18 +54,43 @@ export default class GUI extends React.Component<GUIProps, GUIState> {
     });
   }
 
+  handleTimeLeft() {
+    this.setState({
+      isGameOver: true,
+    })
+  }
+
+  handleMoveDone(side: number) {
+    let currentInitialTime = this.*[side ^ 1];
+
+    this.setState({
+      currentInitialTime: currentInitialTime,
+    });
+  }
+
+  handleChessClockTimerTick(time: number) {
+    this.lastChessClockTime = time;
+  }
+
   render() {
     let bodyElement;
+
 
     return (
       <div>
         <div>
           <Chessboard side={this.player} pieces={this.pieceSetup} chessEngine={this.chessEngine}
-          isHold={this.state.isGameOver} onGameOverEvent={this.handleGameOver}/>
+          isHold={this.state.isGameOver} onGameOverEvent={this.handleGameOver}
+          onMoveDoneEvent={this.handleMoveDone}/>
+          <ChessClock isPaused={false} secondsLimit={15} latestTime={this.lastChessClockTime}
+          onTimeLeftEvent={this.handleTimeLeft} onTimerTickEvent={this.handleChessClockTimerTick} />
         </div>
         <div>
           <button onClick={this.handleNewGameStart}>
             Start a new game
+          </button>
+          <button onClick={this.handleGameSave}>
+            Save game
           </button>
         </div>
       </div>
